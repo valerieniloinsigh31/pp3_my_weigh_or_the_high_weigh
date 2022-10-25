f61b34eeb3bcf37c10027215c70a888b1832bc58
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2022-10-25 10:48:48 +0000

    Added data validation for amount of values input

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-"""API Code"""
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,21 +12,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market")
-    print("Data should be six numbers, separated by commas")
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here:")
-    print(f"The data provided is {data_str}")
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
