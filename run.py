57dd32e16467bedb18898f8bcc1958b649ad914d
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2022-10-29 17:06:17 +0000

    Add user weigh-in data request.

--- run.py
+++ run.py
@@ -23,12 +23,21 @@ SHEET = GSPREAD_CLIENT.open('my_weigh_or_the_high_weigh')
 FUNCTION 1: Using login and password from google sheets
 """
 """
- FUNCTION 2: Access and print weigh-in to date info
- """
+FUNCTION 2: Collect latest weigh-in data from user
+"""
 
-last_weigh_in = SHEET.worksheet('last_weigh_in')
+def get_weighin_data(): 
+    """
+    Get the latest weigh-in values input for each of the six clients from the user. Must be kg (in numerical format and a float). Must have six values
+    """
 
-startdata = last_weigh_in.get_all_values()
+    print ("Please enter the latest weigh-in data for your six clients (in kg)")
+    print ("Must be to two decimal places, separated by commas, for each of the respective clients: Paul, John, James, Declan, Mike,Ian. ")
+    print ("Example: 80.45,75.45,66.32,78.22,76,22,77.13\n")
 
-print(startdata)
+    latest_str = input("Enter latest weigh-in data here:\n")
+    print(f"The latest weights provided for Paul, John, James, Declan, Mike and Ian were {latest_str}")
+
+get_weighin_data()
+
 
